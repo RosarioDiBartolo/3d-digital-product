b9658538af0ce0f12dec769973bffcbfb82d980a
Rosario <marcodibartolo2@gmail.com>
2025-04-12 15:08:28 +0200

    remove admin folder

--- .medusa/admin/assets/chunk-B3XEMIUY-BK-FqrSN.js
+++
@@ -1,1 +0,0 @@
-import{ao as i}from"./index-Caf50g4I.js";var a=i("product","*categories,*shipping_profile,-variants");export{a as P};
--- .medusa/admin/assets/chunk-F6ZOHZVB-DN_G3Mlw.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-Caf50g4I.js";var e=({children:s})=>r.jsx("span",{className:"sr-only",children:s});export{e as V};
